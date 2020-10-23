5af2b220bbe5febbd685430f3554d42e5ea9870e
Mwangi Kabiru <mwangibrian8@gmail.com>
2020-10-23 11:32:10 +0300

    Initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+    apiKey: "AIzaSyCqshTyV5_0qawQqlf9NpxUU_3JIGLaUvE",
+    authDomain: "guestbook-7e542.firebaseapp.com",
+    databaseURL: "https://guestbook-7e542.firebaseio.com",
+    projectId: "guestbook-7e542",
+    storageBucket: "guestbook-7e542.appspot.com",
+    messagingSenderId: "164421736128",
+    appId: "1:164421736128:web:7914d19887a0384f0e7f25",
+    measurementId: "G-ZWQ8W242ZG"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
